9af17188175a0e55de26814283a93d5f5c472e27
Sukhdeepkaur11 <113074268+Sukhdeepkaur11@users.noreply.github.com>
2022-09-18 19:55:30 -0500

    changes to java1

--- java1.js
+++ java1.js
@@ -4,24 +4,21 @@ let age = 32;
 let fav_color = "Pink";
 const dob = "11/07/1988";
 
-console.log(
-  `${name} is ${age} years old, he is born on ${dob} and his favourite color is ${fav_color}.`
-);
-let name1 = "ABC";
+console.log(name + " is " +age  + " years old. She is born on " +dob +" and her favourite color is " +fav_color);
+let name1 = "Vikas"; 
 let age1 = 29;
-let fav_color1 = "Red";
+let fav_color1 = "Blue";
 const dob1 = "17/02/1993";
 
 if (age1 > age) {
-  console.log(`${name1} is older than ${name}`);
+  console.log(name1 + " is older than " +name);
 }
+console.log(name1 + " is younger than " +name);
 age = age + 1;
-console.log(
-  `${name} is ${age} years old, he is born on ${dob} and his favourite color is ${fav_color}.`
-);
+console.log(name +" will be " +age +" years old in 2023 ");
 let sum = age + age1;
-console.log(`Sum of ${name} and ${name1}'s age is 30`);
+console.log(" Sum of " +name +" and " +name1+"'s age is 30");
 
-let a = "20";
-let b = 30;
+let a = "10";
+let b = 20;
 console.log(a + b, b - a, b * a, parseInt(a) + b, parseInt(a) + b.toString());
